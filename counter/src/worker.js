909ab8f23a46fa27f017a0fd75a8c6549fd9f8b5
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2024-01-03 16:46:46 +0100

    add increment option

--- counter/src/worker.js
+++ counter/src/worker.js
@@ -1,28 +1,38 @@
 export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url);
-		if (url.pathname !== "/counter") {
+		const params = new URLSearchParams(url.search);
+		const key = params.get("key");
+		const inc = params.get("inc");
+		if (url.pathname !== "/counter" || !key || !inc) {
 			console.log(url.pathname);
 			return new Response("Not found", { status: 404 });
 		}
-		const params = new URLSearchParams(url.search);
-		const key = params.get("key");
 
 		// Increment the counter in the Cloudflare KV namespace
 		let value = await env.DB.prepare("SELECT * FROM counters WHERE key = ?").bind(key).first();
 		console.log(value);
 		if (value == null) {
-			await env.DB.prepare("INSERT INTO counters (key, value) VALUES (?, ?)").bind(key, 1).run();
-			value = await env.DB.prepare("SELECT * FROM counters WHERE key = ?").bind(key).first();
+			if (inc == 'false') {
+				value = {
+					key: key,
+					value: 0,
+				};
+			} else if (inc == 'true') {
+				await env.DB.prepare("INSERT INTO counters (key, value) VALUES (?, ?)").bind(key, 1).run();
+				value = await env.DB.prepare("SELECT * FROM counters WHERE key = ?").bind(key).first();
+			}
 		} else {
-			await env.DB.prepare("UPDATE counters SET value = value + 1 WHERE key = ?").bind(key).run();
-			value = await env.DB.prepare("SELECT * FROM counters WHERE key = ?").bind(key).first();
+			if (inc == 'true'){
+				await env.DB.prepare("UPDATE counters SET value = value + 1 WHERE key = ?").bind(key).run();
+				value = await env.DB.prepare("SELECT * FROM counters WHERE key = ?").bind(key).first();
+			}
 		}
 
 		return new Response(JSON.stringify(await value), {
 			headers: {
-					'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Origin': '*',
 			},
-	});
+		});
 	},
 };
